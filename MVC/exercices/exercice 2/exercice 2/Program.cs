0c8db4f02b3539c073a2aaac52630edc3910b7c3
Jonathan Mayor <mayorjo@etml.educanet2.ch>
2017-06-07 16:32:34 +0200

    Commit - modification du 07.06.2017 (après-midi)

--- MVC/exercices/exercice 2/exercice 2/Program.cs
+++ MVC/exercices/exercice 2/exercice 2/Program.cs
@@ -1,6 +1,7 @@
 ﻿/*
 * ETML
 * Author : Jonathan Mayor
+* Modificator : YOU 
 * Date : Oula
 * Summary : Programme qui écrit les données d'un tableau
 */
@@ -12,12 +13,13 @@ namespace X_403_mayorjo_Listedeclasse
     {
         static void Main(string[] args)
         {
-            //Tableau 
+            //Tableau avec des données définies à la création ! 
             string[] names = new string[15] {"Balmori", "Colombo", "Dos Santos", "Foot"/*RIP*/, "Gomes", "Guggisberg", "Lopez", "Mayor", "Poget", "Santos Oliveira", "Thomas", "Vallecillos", "Wassenberg", "Pache", "Pittier"};
 
             //Foreach qui permet d'écrire les données du tableau 
             foreach (string name in names)
             {
+                //Affiche le nom 
                 Console.WriteLine(name);
             }
             Console.ReadLine();
