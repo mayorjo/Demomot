ec68e2ef6183db82b2605da262012472a70d5855
Jonathan Mayor <mayorjo@etml.educanet2.ch>
2017-06-14 16:23:45 +0200

    Commit - modification du 14.06.2017
    JDT 
    Regex
    etc.

---
+++ REGEX/exercice pour tester les REGEX/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Variable
+        Regex regexTest = new Regex("cheval");       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Vérification si la valeur match
+            bool regexMatch = regexTest.IsMatch(" ");
+
+            if(regexMatch == true)
+            {
+                panel1.BackColor = Color.Green;
+            }
+
+            else
+            {
+                panel1.BackColor = Color.Red;
+            }
+            
+        }
+    }
+}
